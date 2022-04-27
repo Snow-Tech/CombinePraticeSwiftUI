f8d09e18c2e6c8913bea61c9b3200cb99bca186b
Brian Michael <clay.mrx10@gmail.com>
2022-04-27 10:10:22 +0100

    Initial Commit

---
+++ CombinePraticeSwiftUI/CombinePraticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePraticeSwiftUIApp.swift
+//  CombinePraticeSwiftUI
+//
+//  Created by Brian Hashirama on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CombinePraticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
